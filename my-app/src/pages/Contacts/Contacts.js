d69bb7ed2f20f9cfd8561609205d39e547d71219
SkiPPy43 <skippy4343@yandex.ru>
2022-11-13 21:15:38 +0300

    fix

--- my-app/src/pages/Contacts/Contacts.js
+++ my-app/src/pages/Contacts/Contacts.js
@@ -3,7 +3,7 @@ import React from 'react'
 export function Contacts() {
   return (
     <div>
-      <h1 style={{ margin: "50px", textAlign: "center", color: "green" }}>HELLO!</h1>
+      <h1 style={{ margin: "50px", textAlign: "center", color: "green" }}>Javascript!</h1>
     </div>
   )
 }
